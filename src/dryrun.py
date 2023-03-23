ae320c6d706fb9f9cc62dfa28080bb1c2fd5f17d
ranirk <nagaranigk@gmail.com>
2023-03-23 10:38:28 +0000

    modified spark session

--- src/dryrun.py
+++ src/dryrun.py
@@ -7,8 +7,7 @@ from pyspark.sql.functions import col
 from pyspark.sql import functions as F
 from pyspark.sql.functions import concat,concat_ws
 
-spark = SparkSession.builder.config("spark.jars", "/Users/nagar/Downloads/postgresql-42.5.3.jar") \
-    .master("local").appName("PySpark_Postgres_test").getOrCreate()
+spark = SparkSession.builder.master("local").appName("PySpark_Postgres_test").getOrCreate()
 
 df = spark.read.format("jdbc").option("url",
                                       "jdbc:postgresql://ec2-13-40-49-105.eu-west-2.compute.amazonaws.com:5432/testdb") \
